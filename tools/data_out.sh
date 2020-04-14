95041edd8732a65ad69f322dfbfdcab2991e2fee
Ethan HO <ethan.hobl@gmail.com>
2020-04-14 22:19:44 +1000

    - removed over complexity, start fresh and simple - removed makefile - removed tools - adjusted db tools to data_in/out shell script

---
+++ tools/data_out.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source ../docker/.env
+
+if [ -z "$1" ]
+  then
+    echo "Need to pass in the path to file as a parameter"
+    exit 1
+fi
+
+dir_path="../data_out/"
+source_path=${dir_path}${1}
+target_path=${source_path//'.sql'/'_out.sql'}
+
+source_domain=$LOCAL_DOMAIN
+target_domain=$LIVE_DOMAIN
+
+if [ ! -f "$source_path" ]; then
+    echo "$source_path does not exist"
+    exit 1
+fi
+
+echo "Searching $source_domain in $source_path"
+ccount=$(grep -c "$source_domain" $source_path)
+echo "Count : $ccount"
+if [ $ccount -gt 0 ]
+then
+    sed 's/https:\/\/$source_domain/https:\/\/$target_domain/g' $source_path > $target_path;
+    sed -i '' "s/http:\/\/$source_domain/http:\/\/$target_domain/g" $target_path
+    sed -i '' "s/$source_domain/$target_domain/g" $target_path
+    echo "data in sql at : $target_path"
+else
+    echo "no result, exiting"
+fi
